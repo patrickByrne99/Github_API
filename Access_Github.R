3b31ce8d68a3aa6801a047a7be2a2908434cd50e
Patrick Byrne <patrickbyrne@Patricks-MacBook-Pro.local>
2019-11-06 14:38:03 +0000

    Accessing Data from Github API using R

---
+++ Access_Github.R
@@ -0,0 +1,33 @@
+#install.packages("jsonlite")
+library(jsonlite)
+#install.packages("httpuv")
+library(httpuv)
+#install.packages("httr")
+library(httr)
+
+# Can be github, linkedin etc depending on application
+oauth_endpoints("github")
+
+# Change based on what you 
+myapp <- oauth_app(appname = "Access_Github",
+                   key = "35cadfc1cc0414d6f04d",
+                   secret = "3b86c1d2896b7c733f1d9706d1aeed91851664e3")
+
+# Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+
+# Take action on http error
+stop_for_status(req)
+
+# Extract content from a request
+json1 = content(req)
+
+# Convert to a data.frame
+gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
+
+# Subset data.frame
+gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
