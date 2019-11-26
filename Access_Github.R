0e80dec8f782d46617cd3dd8d52761ee29571bc4
Patrick Byrne <patrickbyrne@Patricks-MacBook-Pro.local>
2019-11-26 15:18:56 +0000

    Functions for analysing a Github user

--- Access_Github.R
+++ Access_Github.R
@@ -4,6 +4,7 @@ library(jsonlite)
 library(httpuv)
 #install.packages("httr")
 library(httr)
+library(plotly)
 
 # Can be github, linkedin etc depending on application
 oauth_endpoints("github")
@@ -18,7 +19,7 @@ github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
 
 # Use API
 gtoken <- config(token = github_token)
-req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+req <- GET("https://api.github.com/users/patrickByrne99/repos", gtoken)
 
 # Take action on http error
 stop_for_status(req)
@@ -30,4 +31,102 @@ json1 = content(req)
 gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
 
 # Subset data.frame
-gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
+gitDF[gitDF$full_name == "patrickByrne99/Github_API", "created_at"] 
+
+
+#Linking to my PlotLy Account
+Sys.setenv("plotly_username"="patrickByrne99")
+Sys.setenv("plotly_api_key"="Bm7lfs63dzZm5LdzvaQg")
+
+#FUNCTIONS--------------------------------------------------------------------------------------
+#Number of followers
+#I assigned some frequently used strings variable names to make my code neater
+userslink = "https://api.github.com/users/"
+followerslink = "/followers"
+followinglink = "/following"
+perpagelink = "?per_page="
+
+numFollowers = function(username)
+{
+  user = jsonlite::fromJSON(paste0(userslink, username))
+  
+  num_followers = user$followers
+  return(num_followers)
+}
+
+#A list of users followers
+followers = function(username)
+{
+  user = jsonlite::fromJSON(paste0(userslink, username, followerslink))
+  list = user$login
+  return(list)
+}
+
+#How many people the user is they Following? 
+numFollowing = function(username)
+{
+  user = jsonlite::fromJSON(paste0(userslink, username))
+  num_following = user$following
+  return(num_following)
+}
+
+#List of above
+following = function(username)
+{
+  userfol = jsonlite::fromJSON(paste0(userslink, username, followinglink))
+  list = userfol$login
+  return(list)
+}
+
+#Location
+location = function(username)
+{
+  user = jsonlite::fromJSON(paste0(userslink, username))
+  location = user$location
+  return(location)
+}
+
+#Account created
+dateCreated = function(username)
+{
+  user = jsonlite::fromJSON(paste0(userslink, username))
+  created = substring(toString(user$created_at), 1, 10)
+  return(created)
+}
+
+#Last activity
+lastActive = function(username)
+{
+  user = jsonlite::fromJSON(paste0(userslink, username))
+  updated = substring(toString(user$updated_at), 1, 10)
+  return(updated)
+}
+
+#Public Repositories Count
+numRepos = function(username)
+{
+  user = jsonlite::fromJSON(paste0(userslink, username))
+  num = user$public_repos
+  return(num)
+}
+
+#List of repositories
+listRepos = function(username)
+{
+  user = jsonlite::fromJSON(paste0(userslink, username))
+  list = jsonlite::fromJSON(user$repos_url)$name
+  
+  return(list)
+}
+
+#Languages of these repositories 
+listLanguages = function(username)
+{
+  user = jsonlite::fromJSON(paste0(userslink, username))
+  list = jsonlite::fromJSON(user$repos_url)$language
+  
+  
+  list = list[!is.na(list)]
+  return(list)
+}
+
