5476ce5d84a660194824047c64c7e44b3c935a8c
Patrick Byrne <patrickbyrne@Patricks-MacBook-Pro.local>
2019-11-27 18:39:36 +0000

    second plot

--- Access_Github.R
+++ Access_Github.R
@@ -167,5 +167,11 @@ plot1
 #sending first plot to plotly
 api_create(plot1, filename = "Repositories vs Followers")
 
+#plot two graphs following vs followers again coloured by year
+plot2 = plot_ly(data = usersDB, x = ~following, y = ~followers, text = ~paste("Followers: ", followers, "<br>Following: ", following), color = ~dateCreated)
+plot2
+
+#sends second plot to plotly
+api_create(plot2, filename = "Following vs Followers")
 
 
